70fd532e3fbfa08de98a8540a395530943010bca
stevenmuh420 <stevenmuh420@gmail.com>
2024-11-20 19:14:51 +0800

    first commit

--- js/script.js
+++ js/script.js
@@ -1,18 +1,28 @@
-let starboy = {
-    profile : [
+let skills = {
+    maximum : 2000000,
+    skill : [
         {
-            "name": "steven muhamad",
-            "skills": ["web developer","cyber security"],
-            "framework" : [
-                "laravel","vue.js","tailwindcss"
-            ],
-            "price" : 1000000,
-            "image" : "image/messi.jpg"
+            "title" : "Laravel Framework",
+            "description" : "Laravel adalah",
+            "image" : "image/laravel.jpeg",
+            "purchase" : 2000000 
+        },
+        {
+            "title" : "Vue.js",
+            "description" : "Vue.js adalah",
+            "image" : "image/vue.png",
+            "purchase" : 3000000 
+        },
+        {
+            "title" : "Tailwindcss",
+            "description" : "Tailwind adalah",
+            "image" : "image/tailwind.jpg",
+            "purchase" : 1000000 
         }
     ]
 }
 // vue
 let app = new Vue({
     el : '#application',
-    data : starboy
+    data : skills
 });
